cfde79b2e6cd8fccbf60051c550038d5809feed8
Jed Fox <git@jedfox.com>
2023-02-02 17:21:00 -0500

    Expand “Getting Started” by default

--- sidebars.js
+++ sidebars.js
@@ -26,7 +26,7 @@ const sidebars = {
     {
       type: 'category',
       label: 'Getting Started',
-      collapsed: true,
+      collapsed: false,
       items: [
         {
           type: 'category',
