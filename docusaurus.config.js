31d0cf6ec3a9eec6618e9cb075eb8918d56a1d08
Rich Howell <github@codenameowl.com>
2023-06-13 06:10:52 +0100

    Update docusaurus.config.js
    Added blog to nav

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -62,6 +62,11 @@ module.exports = {
             label: 'Docs',
           },
           {
+            to: 'blog', 
+            position: 'left',
+            label: 'Blog', 
+          },
+          {
             to: '/contact',
             position: 'left',
             label: 'Contact',
