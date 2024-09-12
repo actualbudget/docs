d776493a40c0cbbab650493f395b28bc3e4a0bc6
Ruben Olsen Lærk <ruben.olsen@dnb.no>
2024-09-12 15:32:30 +0200

    Fixed highlight bug on Reports menu item

--- docs-sidebar.js
+++ docs-sidebar.js
@@ -199,7 +199,6 @@ const sidebars = {
             id: 'reports/index',
           },
           items: [
-            'reports/reports',
             'reports/custom-reports'
           ],
         },
